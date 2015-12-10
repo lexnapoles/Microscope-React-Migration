57b18cf6ef5c5dad8359011b5f2b9edfa2b4dd19
Alejandro Napoles <alejandro.napoles@gmail.com>
2015-12-10 13:24:22 +0100

    Use a method to edit a post.

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -1,22 +1,25 @@
 Template.postEdit.events({
 	'submit form': function(e) {
 		e.preventDefault();
-		
+			
 		var currentPostId = this._id;
 		
 		var postProperties = {
 			url: $(e.target).find('[name=url]').val(),
-			title: $(e.target).find('[name=title]').val()
+			title: $(e.target).find('[name=title]').val(),
 		}
 		
-		Posts.update(currentPostId, {$set: postProperties}, function(error) {
+		Meteor.call('postEdit', currentPostId, postProperties, function(error, result) {
 			if (error) {
-				alert(error.reason);
-			} 
-			else {
-				Router.go('postPage', {_id: currentPostId});
+				return alert(error.reason);
 			}
-		});
+			
+			if (result.postExists) {
+				alert('This link has already been posted');
+			}
+			
+			Router.go('postPage', {_id: result._id});						
+		});	
 	},
 	
 	'click .delete': function(e) {
