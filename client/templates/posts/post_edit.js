053a459dfc1a2c0ccefd684841668075c5bd438c
Alejandro Napoles <alejandro.napoles@gmail.com>
2015-12-15 13:16:08 +0100

    Validate post contents when editing.

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -1,3 +1,17 @@
+Template.postEdit.onCreated(function () {
+	Session.set('postEditErrors', {});
+});
+
+Template.postEdit.helpers({
+	errorMessage: function(field) {
+		return Session.get('postEditErrors')[field];
+	},
+	
+	errorClass: function (field) {
+		return !!Session.get('postEditErrors')[field] ? 'has-error' : '';
+	}
+});
+
 Template.postEdit.events({
 	'submit form': function(e) {
 		e.preventDefault();
@@ -7,6 +21,11 @@ Template.postEdit.events({
 		var postProperties = {
 			url: $(e.target).find('[name=url]').val(),
 			title: $(e.target).find('[name=title]').val(),
+		}
+		
+		var errors = validatePost(postProperties);
+		if (errors.title || errors.url) {
+			return Session.set('postEditErrors', errors);
 		}
 		
 		Meteor.call('postEdit', currentPostId, postProperties, function(error, result) {
