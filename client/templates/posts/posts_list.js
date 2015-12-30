fc41d0e3d52bb3f609d15d6711be963c16b7256c
Alejandro Napoles <alejandro.napoles@gmail.com>
2015-12-30 18:31:36 +0100

    Fade items in when they are drawn.

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,5 +1,18 @@
 Template.postsList.onRendered(function () {
 	this.find('.wrapper')._uihooks = {
+			insertElement: function (node, next) {
+				$(node)
+					.hide()
+					.insertBefore(next)
+					.fadeIn();
+			},
+			
+			removeElement: function (node, next) {
+				$(node).fadeOut(function() {
+					$(this).remove();
+				});
+			},
+			
 			moveElement: function (node, next) {
 				var $node = $(node), $next = $(next);
 				var oldTop = $node.offset().top;
