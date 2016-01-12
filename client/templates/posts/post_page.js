bc771b638f53a5f67dfd4646a1bd80ca2e8cc861
Alejandro Napoles <alejandro.napoles@gmail.com>
2016-01-12 19:09:23 +0100

    Subscribed  to singlePost and comments in the postPage template. Data context is also defined explicitly.

--- client/templates/posts/post_page.js
+++ client/templates/posts/post_page.js
@@ -1,5 +1,18 @@
+Template.postPage.onCreated(function() {
+	Template.instance().id = Router.current().params._id;
+	
+	let template = this;
+	template.autorun(function() {
+		template.subscribe('singlePost', Template.instance().id);
+		template.subscribe('comments', Template.instance().id);
+	});
+});
+
 Template.postPage.helpers({
+	post:  function() { 
+		return Posts.findOne(Template.instance().id); 
+	},
 	comments: function() {
-		return Comments.find({postId: this._id});
+		return Comments.find({postId: Template.instance().id});
 	}
 });
